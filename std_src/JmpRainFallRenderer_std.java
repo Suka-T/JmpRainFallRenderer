908e4c94b457e53ac7830684825b3f72d714e43f
Suka-T <absolute6626x@gmail.com>
2025-06-22 22:08:26 +0900

    プラグインの呼び出し方法を修正。

--- std_src/JmpRainFallRenderer_std.java
+++ std_src/JmpRainFallRenderer_std.java
@@ -1,13 +1,7 @@
-import jmp.JMPLoader;
 import std.StandAlonePluginInvoker;
 
 public class JmpRainFallRenderer_std {
     public static void main(String[] args) {
-        JMPLoader.UsePluginDirectory = false;
-        JMPLoader.UseConfigFile = false;
-        JMPLoader.UseHistoryFile = false;
-        JMPLoader.UseSkinFile = false;
-
-        StandAlonePluginInvoker.exec(args, new JmpRainFallRenderer());
+        StandAlonePluginInvoker.execSimple(args, new JmpRainFallRenderer());
     }
 }
